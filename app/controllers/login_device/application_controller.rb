1c6a1c43fa2a10bf814118c8efae6122654ee457
Anshu Kumar <anshku6@gmail.com>
2019-10-16 20:20:37 +0530

    minor changs

--- app/controllers/login_device/application_controller.rb
+++ app/controllers/login_device/application_controller.rb
@@ -8,8 +8,10 @@ module LoginDevice
       @routes = Engine.routes
     end
     def check_user
+      puts 'on engine file'
       Warden::Manager.after_authentication do |user,auth,opts|
         user.last_login = Time.now
+        puts "on engine file #{user.email}"
       end
     end
   end
