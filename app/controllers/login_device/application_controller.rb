e57565e8bb4bc3ae753023e815ef35934159bae2
Anshu Kumar <anshku6@gmail.com>
2019-10-16 20:31:47 +0530

    minor changes

--- app/controllers/login_device/application_controller.rb
+++ app/controllers/login_device/application_controller.rb
@@ -1,5 +1,5 @@
 module LoginDevice
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
     
     
